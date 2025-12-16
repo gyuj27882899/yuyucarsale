ad457bf40d5bcbc8ed0a218e0b02f9dbb81bcf32
gyuj27882899 <gyuj27882899@gmail.com>
2025-12-16 21:02:00 +0800

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,21 @@
+window.LINE_URL = "https://line.me/R/ti/p/@yuyucarsale";
+
+window.CARS = [
+  {
+    id: "CAR-001",
+    title: "Toyota Prius 2017",
+    image: "./images/car1.jpeg",
+    price: 12500000,
+    currency: "LKR",
+    year: "2017",
+    mileage: "78,000 km",
+    location: "Colombo",
+    tags: ["Hybrid", "Automatic"],
+    spec: [
+      "Engine: 1.8L Hybrid",
+      "Transmission: Automatic",
+      "Color: White",
+      "Condition: Very Good"
+    ]
+  }
+];
